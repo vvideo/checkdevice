3c30d953196f3318ef463d646a6b183b4610f351
Denis Seleznev <hcodes@users.noreply.github.com>
2024-04-05 23:44:30 +0300

    Get lang from url

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,12 @@ import { hit } from 'lyam';
 import { MainPage } from './components/MainPage';
 import { render } from 'preact';
 import { I18NLanguage, setI18nLang } from './i18n/i18n';
+import { getLang } from './utils/getLang';
 
 import './index.css';
 
 hit('95998062');
 
-let lang = ((navigator.language || '').split('-')[0] || 'en') as I18NLanguage;
-
-if (lang !== 'en' && lang !== 'ru') {
-    lang = 'en';
-}
-
-setI18nLang(lang);
+setI18nLang(getLang() as I18NLanguage);
 
 render(MainPage(), document.querySelector('.page') as HTMLDivElement);
