c4e2cf79fda6c4d6700fa3df30bcb8669ba39587
Denis Seleznev <hcodes@users.noreply.github.com>
2024-04-08 14:46:00 +0300

    Fixed unsupported isTypeSupported method

--- src/utils/getTooltip.ts
+++ src/utils/getTooltip.ts
@@ -1,5 +1,6 @@
 import { html } from 'htm/preact';
 import { i18n } from '../i18n/i18n';
+import { VNode } from 'preact';
 
 interface TooltipParams {
     file: boolean;
@@ -12,9 +13,14 @@ export function getTooltip(result: TooltipParams | boolean) {
         return '';
     }
 
+    let isTypeSupportedResult: VNode | string  = result.mediaSource ? i18n('Yes') : i18n('No');
+    if (!window.MediaSource) {
+        isTypeSupportedResult = html`<b>unsupported</b>`;
+    }
+
     return html`
         video.canPlayType(): ${result.file ? i18n('Yes') : i18n('No')}<br />
-        MediaSource.isTypeSupported(): ${result.mediaSource ? i18n('Yes') : i18n('No')}<br />
+        MediaSource.isTypeSupported(): ${isTypeSupportedResult}<br />
         <hr />
         ${result.contentType}
     `;
