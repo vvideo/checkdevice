ce94e270abe9673a291d8f60af9a27aafd80c818
Denis Seleznev <hcodes@users.noreply.github.com>
2024-04-11 18:31:48 +0300

    Fixed padding

--- src/components/ScreenBadge/index.ts
+++ src/components/ScreenBadge/index.ts
@@ -79,7 +79,7 @@ export function ScreenBadge(props: ScreenBadge) {
                 click: true,
                 background: 'gold',
                 top: {
-                    text: props.isHdr ? html`<b>HDR</b>` : '',
+                    text: props.isHdr ? html`<b>HDR</b>` : '\u00A0',
                 },
                 bottom: {
                     text: screenText,
