ebba07ead578825a40306efa2bb4c3e996f06992
Denis Seleznev <hcodes@users.noreply.github.com>
2024-04-29 10:31:50 +0300

    Remove unused code

--- src/utils/getHcpVersion.ts
+++ src/utils/getHcpVersion.ts
@@ -1,27 +1,5 @@
-import { CheckHdcpVersion } from 'hdcp';
+import { CheckHdcpVersion, getMaxHdcpVersion } from 'hdcp';
 import { i18n } from '../i18n/i18n';
-
-export function getMaxHdcpVersion(versions: CheckHdcpVersion[]) {
-    for (let i = versions.length - 1; i >= 0; i--) {
-        const item = versions[i];
-        if (item.status === 'usable') {
-            return item.version;
-        }
-    }
-
-    return '';
-}
-
-export const HDCP_VERSION_WITH_4K = 2.2;
-export function is4kHdcpSupported(versions: CheckHdcpVersion[]) {
-    const maxVersion = getMaxHdcpVersion(versions);
-
-    if (!maxVersion) {
-        return false;
-    }
-
-    return parseFloat(maxVersion) >= HDCP_VERSION_WITH_4K;
-}
 
 export function getHdcpNotDetected() {
     return i18n('Not detected');
