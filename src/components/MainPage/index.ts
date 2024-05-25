652bf5bcbe0f976eb778547a909ca3d35aebb822
Denis Seleznev <hcodes@users.noreply.github.com>
2024-05-26 01:49:33 +0300

    Removed unused console.log

--- src/components/MainPage/index.ts
+++ src/components/MainPage/index.ts
@@ -30,7 +30,6 @@ export function MainPage() {
 
     useEffect(() => {
         function onVisibilityСhange() {
-            console.log('onVisibilityСhange');
             forceUpdate();
         }
 
