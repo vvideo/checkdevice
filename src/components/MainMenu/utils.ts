fe71f39cfdddad202101ec64a9970fa3bf55b239
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-02 00:34:43 +0300

    Fix for github pages

--- src/components/MainMenu/utils.ts
+++ src/components/MainMenu/utils.ts
@@ -1,5 +1,8 @@
 export function getIdFromLocation() {
-    const id = window.location.pathname.split(/[?.\/]/)[1];
+    const id = window.location.pathname
+        // Fix for github pages
+        .replace(/^\/caniwatchvideo/, '')
+        .split(/[?.\/]/)[1];
 
     return id ? id : 'index';
 }
