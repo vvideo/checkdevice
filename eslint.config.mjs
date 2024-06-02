8e422127367dfd0d2790420e6194b57e90bb4e12
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-03 00:01:38 +0300

    :cyclone:

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      'dist/**',
+      'rollup.config.mjs'
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: "^_"
+        }
+      ]
+    }
+  }
+];
