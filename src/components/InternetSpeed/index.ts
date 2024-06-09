210196579577e36aa9b32d1a6ad1219992864d5b
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-09 13:56:47 +0300

    Change position for Интернетометр

--- src/components/InternetSpeed/index.ts
+++ src/components/InternetSpeed/index.ts
@@ -9,7 +9,7 @@ const b = block('internet-speed');
 export function InternetSpeed() {
     return html`<ul class="${b()}">
         <li><a href="https://speedtest.net" target="_blank">Speedtest</a></li>
-        <li><a href="https://fast.com" target="_blank">Fast</a></li>
         ${getI18nLang() === 'ru' ? html`<li><a href="https://ya.ru/internet" target="_blank">Интернетометр</a></li>` : ''}
+        <li><a href="https://fast.com" target="_blank">Fast</a></li>
     </ul>`;
 }
