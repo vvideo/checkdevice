2df014203dbb746f7476d38b3d8bdf5f77146581
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-16 15:53:17 +0300

    Fix CSS styles

--- src/components/QuestionSurroundSound/index.ts
+++ src/components/QuestionSurroundSound/index.ts
@@ -5,6 +5,11 @@ import { Result } from '../Result';
 import { Codec } from '../Codec';
 import { i18n } from '../../i18n/i18n';
 import { CodecDetails } from '../CodecDetails';
+import { block } from '../../utils/bem';
+
+import './index.css';
+
+const b = block('question-surround-sound');
 
 export function QuestionSurroundSound() {
     const isDolbyDigital = isDolbyDigitalSupported();
@@ -24,64 +29,66 @@ export function QuestionSurroundSound() {
     const head = html`${i18n('Can I listen to surround sound on online services?')}\u00a0<${Result} value="${mainAnswer}"><//>`;
 
     return html`
-        <${ActiveQuestion} head="${head}">
-            ${i18n('Supports one of the audio codecs?')}\u00a0<${Result} value="${mainAnswer}"><//>
-            <ul>
-                <li>
-                    <${Codec}
-                        name="Dolby Digital"
-                        color="black"
-                        border="white"
-                        disabled="${!isDolbyDigital.any}"
-                        tooltip="${CodecDetails(isDolbyDigital)}">
-                        <//>\u00a0<${Result} value="${isDolbyDigital.any}"><//>
-                </li>
-                <li>
-                    <${Codec}
-                        name="Dolby Digital Plus"
-                        color="black"
-                        border="white"
-                        disabled="${!isDolbyDigitalPlus.any}"
-                        tooltip="${CodecDetails(isDolbyDigitalPlus)}">
-                        <//>\u00a0<${Result} value="${isDolbyDigitalPlus.any}"><//>
-                </li>
-                <li>
-                    <${Codec}
-                        name="Dolby Atmos"
-                        color="black"
-                        border="white"
-                        disabled="${!isDolbyAtmos}">
-                        <//>\u00a0<${Result} value="${isDolbyAtmos}"><//>
-                </li>
-                <li>
-                    <${Codec}
-                        name="DTS"
-                        color="black"
-                        border="white"
-                        disabled="${!isDts.any}"
-                        tooltip="${CodecDetails(isDts)}">
-                        <//>\u00a0<${Result} value="${isDts.any}"><//>
-                </li>
-                <li>
-                    <${Codec}
-                        name="DTS:HD"
-                        color="black"
-                        border="white"
-                        disabled="${!isDtsHd.any}"
-                        tooltip="${CodecDetails(isDtsHd)}">
-                        <//>\u00a0<${Result} value="${isDtsHd.any}"><//>
-                </li>
-                <li>
-                    <${Codec}
-                        name="DTS:X"
-                        color="black"
-                        border="white"
-                        disabled="${!isDtsX.any}"
-                        tooltip="${CodecDetails(isDtsX)}">
-                        <//>\u00a0<${Result} value="${isDtsX.any}"><//>
-                </li>
-            </ul>
-        <//>
+        <div class="${b()}">
+            <${ActiveQuestion} head="${head}">
+                ${i18n('Supports one of the audio codecs?')}\u00a0<${Result} value="${mainAnswer}"><//>
+                <ul>
+                    <li>
+                        <${Codec}
+                            name="Dolby Digital"
+                            color="black"
+                            border="white"
+                            disabled="${!isDolbyDigital.any}"
+                            tooltip="${CodecDetails(isDolbyDigital)}">
+                            <//>\u00a0<${Result} value="${isDolbyDigital.any}"><//>
+                    </li>
+                    <li>
+                        <${Codec}
+                            name="Dolby Digital Plus"
+                            color="black"
+                            border="white"
+                            disabled="${!isDolbyDigitalPlus.any}"
+                            tooltip="${CodecDetails(isDolbyDigitalPlus)}">
+                            <//>\u00a0<${Result} value="${isDolbyDigitalPlus.any}"><//>
+                    </li>
+                    <li>
+                        <${Codec}
+                            name="Dolby Atmos"
+                            color="black"
+                            border="white"
+                            disabled="${!isDolbyAtmos}">
+                            <//>\u00a0<${Result} value="${isDolbyAtmos}"><//>
+                    </li>
+                    <li>
+                        <${Codec}
+                            name="DTS"
+                            color="black"
+                            border="white"
+                            disabled="${!isDts.any}"
+                            tooltip="${CodecDetails(isDts)}">
+                            <//>\u00a0<${Result} value="${isDts.any}"><//>
+                    </li>
+                    <li>
+                        <${Codec}
+                            name="DTS:HD"
+                            color="black"
+                            border="white"
+                            disabled="${!isDtsHd.any}"
+                            tooltip="${CodecDetails(isDtsHd)}">
+                            <//>\u00a0<${Result} value="${isDtsHd.any}"><//>
+                    </li>
+                    <li>
+                        <${Codec}
+                            name="DTS:X"
+                            color="black"
+                            border="white"
+                            disabled="${!isDtsX.any}"
+                            tooltip="${CodecDetails(isDtsX)}">
+                            <//>\u00a0<${Result} value="${isDtsX.any}"><//>
+                    </li>
+                </ul>
+            <//>
+        </div>
     `;
 }
 
