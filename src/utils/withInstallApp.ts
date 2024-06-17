2f0c85f0ae7d42ca6911187e25d4fdab7916dba2
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-18 00:48:10 +0300

    Fix ts warning

--- src/utils/withInstallApp.ts
+++ src/utils/withInstallApp.ts
@@ -1,4 +1,5 @@
 export function withInstallApp() {
+    // @ts-ignore
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let deferredPrompt;
     window.addEventListener('beforeinstallprompt', e => {
