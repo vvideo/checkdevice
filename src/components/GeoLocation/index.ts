b782831708f01a99e273d4c65cc5b8888a609945
Denis Seleznev <hcodes@users.noreply.github.com>
2024-06-21 00:17:37 +0300

    Add error message for geo location

--- src/components/GeoLocation/index.ts
+++ src/components/GeoLocation/index.ts
@@ -5,6 +5,7 @@ import { TreeList } from '../TreeList';
 import { YaStaticMap } from '../YaStaticMap';
 import { block } from '../../utils/bem';
 import { i18n } from '../../i18n/i18n';
+import { Spinner } from '../Spinner';
 
 import './index.css';
 
@@ -20,6 +21,8 @@ export function GeoLocation(props: GeoLocationProps) {
     }
 
     const [coords, setCoords] = useState<GeolocationCoordinates | null>(null);
+    const [inProgress, setInProgress] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     const handleClick = useCallback(() => {
         const options = {
@@ -27,6 +30,8 @@ export function GeoLocation(props: GeoLocationProps) {
             timeout: 5000,
             maximumAge: 0,
         };
+
+        setInProgress(true);
 
         function success(position: GeolocationPosition) {
             const { coords } = position;
@@ -40,9 +45,13 @@ export function GeoLocation(props: GeoLocationProps) {
                 speed: coords.speed,
                 heading: coords.heading,
             });
+
+            setInProgress(false);
         }
 
         function error(error: GeolocationPositionError) {
+            setInProgress(false);
+            setError(error.message);
             console.error(error);
         }
 
@@ -50,8 +59,9 @@ export function GeoLocation(props: GeoLocationProps) {
     }, []);
 
     return html`
-        <${Button} theme="active" onClick="${handleClick}">${i18n('Request geo location')}<//>
+        <${Button} theme="active" onClick="${handleClick}">${i18n('Request geo location')}<//> ${inProgress ? html`<${Spinner} size="m" //>` : ''}
         ${coords ? html`<div class="${b('list')}"><${TreeList} data="${coords}" //></div>` : ''}
+        ${error ? html`<div class="${b('error')}">${error}</div>` : ''}
         ${coords ? html`<div class="${b('map')}">
             <${YaStaticMap}
                 latitude="${coords.latitude}"
