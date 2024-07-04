f246fb9871ad2192c9376219c3bab90a9a1ad72d
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-04 10:43:02 +0300

    Update footer link

--- src/components/Footer/index.ts
+++ src/components/Footer/index.ts
@@ -1,5 +1,6 @@
 import { html } from 'htm/preact';
 import { block } from '../../utils/bem';
+import { i18n } from '../../i18n/i18n';
 
 import './index.css';
 
@@ -8,6 +9,6 @@ const b = block('footer');
 export function Footer() {
     return html`<div class="${b()}">
         <div class="${b('item')}">© <span class="${b('l1')}">V</span><span class="${b('l2')}">v</span><span class="${b('l3')}">i</span><span class="${b('l4')}">d</span><span class="${b('l5')}">e</span><span class="${b('l6')}">o</span></div>
-        <div class="${b('item', { g: true })}"><a target="_blank" href="https://github.com/vvideo/caniwatchvideo"><span class="${b('G')}">G</span><span class="${b('i')}">i</span><span class="${b('t')}">t</span><span class="${b('H')}">H</span><span class="${b('u')}">u</span><span class="${b('b')}">b</span></a></div>
+        <div class="${b('item', { g: true })}"><a target="_blank" href="https://github.com/vvideo/caniwatchvideo/issues">${i18n('Report a bug')}</a></div>
     </div>`;
 }
