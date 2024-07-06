84f9471f4d4989ed6cbc2a01f67f62ce009b6d5f
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-06 10:36:09 +0300

    Add input

---
+++ src/components/Input/index.ts
@@ -0,0 +1,34 @@
+import { html } from 'htm/preact';
+import { useCallback, useRef, useState } from 'preact/hooks';
+import { block } from '../../utils/bem';
+import { classname } from '../../utils/classname';
+
+import './index.css';
+
+interface InputProps {
+    className?: string;
+    value?: string;
+    placeholder?: string;
+    onChange?: (value: string) => void;
+}
+
+const b = block('input');
+
+export function Input(props: InputProps) {
+    const className = classname(b(), props.className);
+    const [value, setValue] = useState<string>(props.value || '');
+
+    const ref = useRef<HTMLInputElement>();
+
+    const handleInput = useCallback(() => {
+        if (!ref.current) {
+            return;
+        }
+
+        const value = ref.current.value;
+        setValue(value);
+        props.onChange && props.onChange(value);
+    }, [value, props.onChange]);
+
+    return html`<input ref="${ref}" class="${className}" onInput="${handleInput}" placeholder="${props.placeholder}" value="${value}" />`
+}
