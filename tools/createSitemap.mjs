efc1cf93f274d5809fd8a6df9e46b812513bf423
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-07 01:49:14 +0300

    Fix sitemap

--- tools/createSitemap.mjs
+++ tools/createSitemap.mjs
@@ -1,5 +1,6 @@
 export function createSitemap(pages) {
-    return `<urlset>
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${
     pages.map(item => {
         return `  <url>
