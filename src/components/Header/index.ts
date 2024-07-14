a1a5c7d8f285316488f52c0940df20c050f44215
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-14 15:49:04 +0300

    Add link for header

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,6 +1,7 @@
 import { html } from 'htm/preact';
 import { block } from '../../utils/bem';
 import { i18n } from '../../i18n/i18n';
+import { Link } from '../Link';
 
 import './index.css';
 
@@ -9,7 +10,7 @@ const b = block('header');
 export function Header() {
     return html`
         <header class="${b()}">
-            <span class="${b('logo')}"></span>${i18n('Check device online')}
+            <${Link} theme="white" href="/"><span class="${b('logo')}"></span>${i18n('Check device online')}<//>
         </header>
     `;
 }
