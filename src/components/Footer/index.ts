542d064fccee6bc57c7d2aa7a1409b41134ae6fd
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-27 18:15:50 +0300

    Fix CSS

--- src/components/Footer/index.ts
+++ src/components/Footer/index.ts
@@ -10,7 +10,7 @@ const b = block('footer');
 
 export function Footer() {
     return html`<footer class="${b()}">
-        <div class="${b('item', { report: true })}"><${Link} target="_blank" href="https://github.com/vvideo/caniwatchvideo/issues/new">${i18n('Report a bug')}<//></div>
+        <div class="${b('item', { report: true })}"><${Link} theme="white" target="_blank" href="https://github.com/vvideo/caniwatchvideo/issues/new">${i18n('Report a bug')}<//></div>
         <div class="${b('item')}"><${LangSwitcher} //></div>
         <div class="${b('item')}">© Vvideo</div>
     </footer>`;
