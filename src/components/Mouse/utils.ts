8d7acd62bccac33339cc4f6b3bd3dd127558e49e
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-04 21:48:38 +0300

    Fix wheel direction

--- src/components/Mouse/utils.ts
+++ src/components/Mouse/utils.ts
@@ -1,5 +1,22 @@
 import { isMacintosh } from '../../utils/platform';
 
+const MAX_WHEELDELTA = 10;
+
 export function prepareDeltaY(event: WheelEvent): number {
-    return (event.deltaMode ? 10 : event.deltaY) * (isMacintosh() ? -1 : 1)
+    const coef = isMacintosh() ? -1 : 1;
+    let { deltaY } = event;
+
+    if (!event.deltaMode ) {
+        if (deltaY > MAX_WHEELDELTA) {
+            deltaY = MAX_WHEELDELTA;
+        }
+
+        if (deltaY < -MAX_WHEELDELTA) {
+            deltaY = -MAX_WHEELDELTA;
+        }
+
+        return deltaY * coef;
+    }
+
+    return (deltaY > 0 ? MAX_WHEELDELTA : -MAX_WHEELDELTA) * coef;
 }
