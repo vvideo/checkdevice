3b40b1a0d7dd3f00159a220a60fa5cceef84b199
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-06 20:56:56 +0300

    Add usb component

---
+++ src/components/Usb/index.ts
@@ -0,0 +1,73 @@
+import { html } from 'htm/preact';
+import { useCallback, useState } from 'preact/hooks';
+import { block } from '../../utils/bem';
+import { i18n } from '../../i18n/i18n';
+import { Button } from '../Button';
+import { WarningMessage } from '../WarningMessage';
+
+import './index.css';
+import { TreeList } from '../TreeList';
+
+const b = block('usb');
+
+export function Usb() {
+    const [usbDevice, setUsbDevice] = useState<any | null>(null);
+    const [error, setError] = useState<Error | null>(null);
+
+    const handleClick = useCallback(() => {
+        navigator.usb.requestDevice({ filters: [] }).then(device => {
+            setError(null);
+
+            const result = {};
+            for (const key in device) {
+                // @ts-ignore
+                result[key] = device[key];
+                console.log(key, device.hasOwnProperty(key));
+            }
+
+            setUsbDevice(result);
+            console.log(result);
+        }).catch(error => {
+            setError(error);
+            console.log(error);
+        });
+    }, []);
+
+    if (!navigator.usb || typeof navigator.usb.requestDevice !== 'function') {
+        return html`<${WarningMessage}>${i18n('WebUSB API is not supported.')}<//>`;
+    }
+
+    return html`<div class="${b()}">
+        <div class="${b('controls')}">
+            <${Button} theme="active" onClick="${handleClick}">${i18n('Select USB device')}<//>
+        </div>
+
+        <${UsbError} error="${error}" //>
+
+        ${usbDevice ? html`<div class="${b('info')}">
+            <${TreeList} data="${usbDevice}" //>
+        </div>` : ''}
+    </div>`;
+}
+
+interface UsbErrorProps {
+    error: Error;
+}
+
+function UsbError(props: UsbErrorProps) {
+    const { error } = props;
+
+    if (!error) {
+        return '';
+    }
+    
+    if (error.name === 'NotFoundError') {
+        return html`<${WarningMessage}>${i18n('USB device not found.')}<//>`;
+    }
+
+    if (error.name === 'NotAllowedError') {
+        return html`<${WarningMessage}>${i18n('USB device is blocked.')}<//>`;
+    }
+
+    return html`<${WarningMessage}>${error.message}<//>`;
+}
