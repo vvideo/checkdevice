ac1abe3aa85313a0a71cde09cc6a7ca4bb43e418
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-10 10:19:58 +0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
     yaMapsApiKey: '82d7a38b-bad1-4b72-b66c-bfdaf838ba66',
     showJSErrorRepportUrl: 'https://github.com/hcodes/show-js-error/issues/new?title={title}&body={body}',
-    metrikaCounterId: '95998062',// 97747983
+    metrikaCounterId: '95998062', // 97747983
 };
