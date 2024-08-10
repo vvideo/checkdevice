e218f25f1baf874701ecdba1043f6f1f90c2f86c
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-10 11:40:06 +0300

    Save photo from camera

--- src/components/Camera/index.ts
+++ src/components/Camera/index.ts
@@ -2,7 +2,7 @@ import { html } from 'htm/preact';
 import { useCallback, useRef, useState } from 'preact/hooks';
 import { block } from '../../utils/css/bem';
 import { i18n } from '../../i18n';
-import { getConstraints, requestCamera, stopCamera } from './utils';
+import { getConstraints, requestCamera, stopCamera, savePhoto } from './utils';
 import { Button } from '../Button';
 import { getSelectedButton, RadioButtons } from '../RadioButtons';
 import { RadioButtonProps } from '../RadioButton';
@@ -11,9 +11,9 @@ import { CameraInfo } from '../CameraInfo';
 import { Checkbox } from '../Checkbox';
 import { CameraError } from '../CameraError';
 import { getStreamParams } from '../../utils/getStreamParams';
+import { isSsr } from '../../utils/isSsr';
 
 import './index.css';
-import { isSsr } from '../../utils/isSsr';
 
 const b = block('camera');
 
@@ -63,6 +63,15 @@ export function Camera() {
         });
     }, [stream, quality, withMic]);
 
+    const handleSavePhoto = useCallback(() => {
+        if (refVideo.current) {
+            savePhoto(
+                refVideo.current,
+                `camera_photo_${new Date().toISOString().split('T')[0]}.png`
+            );
+        }
+    }, [])
+
     const handleSelect = useCallback((value: string) => {
         const video = refVideo.current;
 
@@ -102,7 +111,7 @@ export function Camera() {
             <${Button} class="${b('select-camera')}" theme="${showStop ? 'red' : 'active'}" onClick="${handleClick}">${stream ? i18n('Stop') : i18n('Check camera')}<//>
             <${Checkbox} title="${i18n('On/off microphone')}" label="${i18n('Mic')}" checked="${withMic}" onClick="${handleMic}" //>
         </div>
-        <div class="${b('controls')}">
+        <div class="${b('top-controls')}">
             <${RadioButtons} hideLabel label="${i18n('Select camera option')}" onSelect="${handleSelect}" buttons="${buttons}"><//>
         </div>
 
@@ -111,6 +120,10 @@ export function Camera() {
         <div class="${b('container', { played })}">
             <video ref="${refVideo}" class="${b('video')}" />
         </div>
+
+        ${showStop ? html`<div class="${b('bottom-controls')}">
+            <${Button} onClick="${handleSavePhoto}">${'Save photo'}<//>
+        </div>` : ''}
 
         <div class="${b('params')}">
             ${params ? html`<${CameraInfo} ...${params} //>` : ''}
--- src/components/Camera/utils.ts
+++ src/components/Camera/utils.ts
@@ -46,3 +46,19 @@ export function getConstraints(value: string, audio: boolean): MediaStreamConstr
         },
     }[value];
 }
+
+export function savePhoto(video: HTMLVideoElement, fileName: string) {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+        ctx.drawImage(video, 0, 0);
+    }
+
+    const downloadLink = document.createElement('a');
+    downloadLink.download = fileName;
+    downloadLink.href = canvas.toDataURL('image/png');
+    downloadLink.click();    
+}
