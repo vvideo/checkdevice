54e4843b62a04c6b65cb2924201a6afdb11c6a80
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-24 22:55:33 +0300

    Add output name and id device

--- src/pages/BluetoothPage/components/Bluetooth/index.tsx
+++ src/pages/BluetoothPage/components/Bluetooth/index.tsx
@@ -8,22 +8,25 @@ import { BluetoothError } from '../BluetoothError';
 import { Spinner } from '../../../../components/ui/Spinner';
 import { WarningMessage } from '../../../../components/ui/WarningMessage';
 import { isSsr } from '../../../../utils/isSsr';
+import { List } from '../../../../components/ui/List';
+
+import './index.css';
 
 const b = block('bluetooth');
 
 export function Bluetooth() {
     const [ error, setError ] = useState<Error | null>(null);
     const [ disabled, setDisabled ] = useState(false);
+    const [ device, setDevice ] = useState<any>(null);
 
     const handleClick = useCallback(() => {
         setDisabled(true);
-        
+
         // @ts-ignore
-        navigator.bluetooth.requestDevice({ acceptAllDevices: true }).then(data => {
+        navigator.bluetooth.requestDevice({ acceptAllDevices: true }).then(bluetoothDevice => {
             setDisabled(false);
             setError(null);
-
-            console.log('Bluetooth device', data);
+            setDevice(bluetoothDevice);
         }).catch((error: any) => {
             setDisabled(false);
             setError(error);
@@ -35,8 +38,22 @@ export function Bluetooth() {
         return (<WarningMessage>{i18n('Web Bluetooth API is not supported.')}</WarningMessage>)
     }
 
+    const items: [string, string][] = [];
+    if (device) {
+        items.push([
+            'Name',
+            device.name,
+        ], [
+            'Id',
+            device.id,
+        ]);
+    }
+
     return (<div class={b()}>
         <Button disabled={disabled} theme="active" onClick={handleClick}><span>{disabled ? (<Spinner size="s" />) : null} {i18n('Request')}</span></Button>
+
+        <List class={b('list')} items={items} />
+
         <BluetoothError error={error} />
     </div>);
 }
