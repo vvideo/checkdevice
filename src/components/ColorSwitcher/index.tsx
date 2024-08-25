1a0290d3c490533010ad76b2424c9ed79fab3748
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-25 13:53:31 +0300

    Add ColorSwitcher component

---
+++ src/components/ColorSwitcher/index.tsx
@@ -0,0 +1,48 @@
+import { h } from 'preact';
+
+import { block } from '../../utils/css/bem';
+import { useCallback, useState } from 'preact/hooks';
+import { ColorSwitcherItem } from '../ColorSwitcherItem';
+
+import './index.css';
+
+interface ColorSwitcherProps {
+    label?: string;
+    title?: string;
+    selected?: string;
+    items: {
+        title?: string;
+        value: string;
+        color: string;
+    }[];
+}
+
+const b = block('color-switcher');
+
+export function ColorSwitcher(props: ColorSwitcherProps) {
+    const [ value, setValue ] = useState<string>(props.selected || props.items[0].value);
+
+    const handleClick = useCallback((data: string) => {
+        setValue(data);
+    }, [setValue]);
+
+    return (
+        <div class={b()}>
+            {props.label ?(<div class={b('label')} title={props.title}>{props.label}</div>) : null }
+            <ul class={b('list')}>
+            {props.items.map(item => {
+                return (
+                    <ColorSwitcherItem
+                        onClick={handleClick}
+                        key={item.value}
+                        color={item.color}
+                        title={item.title}
+                        value={item.value}
+                        selected={item.value === value }
+                    />
+                );
+            })}
+            </ul>
+        </div>
+    );
+}
---
+++ src/components/ColorSwitcherItem/index.tsx
@@ -0,0 +1,29 @@
+import { h } from 'preact';
+import { useCallback } from 'preact/hooks';
+
+import { block } from '../../utils/css/bem';
+
+import './index.css';
+
+const b = block('color-switcher-item');
+
+interface ColorSwitcherItemProps {
+    color: string;
+    title?: string;
+    value: string;
+    selected: boolean;
+
+    onClick: (value: string) => void;
+}
+
+export function ColorSwitcherItem(props: ColorSwitcherItemProps) {
+    const handleClick = useCallback(() => {
+        props.onClick(props.value);
+    }, []);
+
+    return (<li
+        class={b({ selected: props.selected })}
+    >
+        <div onClick={handleClick} title={props.title} class={b('color')} style={`background-color: ${props.color}`}></div>
+    </li>);
+}
