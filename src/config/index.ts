952e8dd47262fbba18c3fcea4a478bc0a4e76afc
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-07 11:48:37 +0300

    :cyclone:

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,11 @@
 export const config = {
     yaMapsApiKey: '82d7a38b-bad1-4b72-b66c-bfdaf838ba66',
     showJSErrorRepportUrl: 'https://github.com/hcodes/show-js-error/issues/new?title={title}&body={body}',
-    metrikaCounterId: '95998062', // 97747983
+    metrikaCounterId: 95998062, // 97747983
+    metrikaOptions: {
+        clickmap: false,
+        trackLinks: true,
+        trackHash: true,
+        accurateTrackBounce: true,
+    },
 };
