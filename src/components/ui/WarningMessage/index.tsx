9b5881d864151726ab42dad7d5799ad6a3a87220
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-13 11:08:37 +0300

    Add class prop

--- src/components/ui/WarningMessage/index.tsx
+++ src/components/ui/WarningMessage/index.tsx
@@ -2,14 +2,17 @@ import { h } from 'preact';
 import { block } from '../../../utils/css/bem';
 
 import './index.css';
+import { classname } from '../../../utils/css/classname';
 
 interface WarningMessageProps {
-    theme?: 'gray'
+    theme?: 'gray';
+    class?: string;
     children: h.JSX.Element | string;
 }
 
 const b = block('warning-message');
 
 export function WarningMessage(props: WarningMessageProps) {
-    return (<span class={b({ theme: props.theme })}>⚠️ {props.children}</span>);
+    const cls = classname(b({ theme: props.theme }), props.class);
+    return (<span class={cls}>⚠️ {props.children}</span>);
 }
