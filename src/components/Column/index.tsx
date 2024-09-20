444edcca6b548bc8ec7d90c7d03e5ba84690673f
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-20 09:05:45 +0300

    Add class prop

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 
 import { block } from '../../utils/css/bem';
+import { classname } from '../../utils/css/classname';
 
 import './index.css';
 
@@ -8,13 +9,15 @@ const b = block('column');
 
 interface ColumnProps {
     name: string;
+    class?: string;
     title?: string;
     children: h.JSX.Element | h.JSX.Element[] | string;
     disabled?: boolean;
 }
 
 export function Column(props: ColumnProps) {
-    return (<div class={b()}>
+    const cls = classname(b(), props.class);
+    return (<div class={cls}>
         <div title={props.title} class={b('category', { disabled: props.disabled })}>{props.name}</div>
         <div class={b('body')}>{props.children}</div>
     </div>);
