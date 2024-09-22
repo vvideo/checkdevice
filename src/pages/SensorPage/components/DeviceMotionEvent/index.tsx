64e28e7ca7fd19a539452de46f291e3d809632c3
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-22 10:56:30 +0300

    Add DeviceMotionEvent component

---
+++ src/pages/SensorPage/components/DeviceMotionEvent/index.tsx
@@ -0,0 +1,58 @@
+import { h } from 'preact';
+
+import { block } from '../../../../utils/css/bem';
+import { useEffect, useState } from 'preact/hooks';
+import { i18n } from '../../../../i18n';
+import { List } from '../../../../components/ui/List';
+
+const b = block('device-motion-event');
+
+export function DeviceMotionEvent() {
+    const [ motionEvent, setMotionEvent ] = useState<DeviceMotionEvent | null>(null);
+
+    useEffect(() => {
+        function handleDeviceMotion(event: DeviceMotionEvent) {
+            setMotionEvent(event);
+        }
+
+        window.addEventListener('devicemotion', handleDeviceMotion, true);
+
+        return () => {
+            window.removeEventListener('devicemotion', handleDeviceMotion, true);
+        };
+    }, []);
+
+    const accelerationItems: [string, number | null | undefined][] = motionEvent?.acceleration ?[
+        ['x: ', motionEvent.acceleration?.x],
+        ['y: ', motionEvent.acceleration?.y],
+        ['z: ', motionEvent.acceleration?.z],
+    ] : [];
+
+    const accelerationIncludingGravityItems: [string, number | null | undefined][] = motionEvent?.acceleration ?[
+        ['x: ', motionEvent.accelerationIncludingGravity?.x],
+        ['y: ', motionEvent.accelerationIncludingGravity?.y],
+        ['z: ', motionEvent.accelerationIncludingGravity?.z],
+    ] : [];
+
+    const rotationRateItems: [string, number | null | undefined][] = motionEvent?.rotationRate ?[
+        ['alpha: ', motionEvent.rotationRate?.alpha],
+        ['beta: ', motionEvent.rotationRate?.beta],
+        ['gamma: ', motionEvent.rotationRate?.gamma],
+    ] : [];
+
+    return (
+        <div class={b()}>
+            {accelerationItems.length ? (
+                <List title={i18n('Acceleration')} items={accelerationItems} />
+            ) : null}
+
+            {accelerationIncludingGravityItems.length ? (
+                <List title={i18n('Acceleration including gravity')} items={accelerationItems} />
+            ) : null}
+
+            {rotationRateItems.length ? (
+                <List title={i18n('Rotation rate')} items={rotationRateItems} />
+            ) : null}
+        </div>
+    );
+}
--- src/pages/SensorPage/components/Sensor/index.tsx
+++ src/pages/SensorPage/components/Sensor/index.tsx
@@ -1,58 +1,25 @@
 import { h } from 'preact';
 
 import { block } from '../../../../utils/css/bem';
-import { useEffect, useState } from 'preact/hooks';
+import { DeviceMotionEvent } from '../DeviceMotionEvent';
+import { isSsr } from '../../../../utils/isSsr';
 import { i18n } from '../../../../i18n';
-import { List } from '../../../../components/ui/List';
+import { WarningMessage } from '../../../../components/ui/WarningMessage';
 
 const b = block('sensor');
 
 export function Sensor() {
-    const [ motionEvent, setMotionEvent ] = useState<DeviceMotionEvent | null>(null);
+    if (isSsr) {
+        return null;
+    }
 
-    useEffect(() => {
-        function handleDeviceMotion(event: DeviceMotionEvent) {
-            setMotionEvent(event);
-        }
-
-        window.addEventListener('devicemotion', handleDeviceMotion, true);
-
-        return () => {
-            window.removeEventListener('devicemotion', handleDeviceMotion, true);
-        };
-    }, []);
-
-    const accelerationItems: [string, number | null | undefined][] = motionEvent?.acceleration ?[
-        ['x: ', motionEvent.acceleration?.x],
-        ['y: ', motionEvent.acceleration?.y],
-        ['z: ', motionEvent.acceleration?.z],
-    ] : [];
-
-    const accelerationIncludingGravityItems: [string, number | null | undefined][] = motionEvent?.acceleration ?[
-        ['x: ', motionEvent.accelerationIncludingGravity?.x],
-        ['y: ', motionEvent.accelerationIncludingGravity?.y],
-        ['z: ', motionEvent.accelerationIncludingGravity?.z],
-    ] : [];    
-
-    const rotationRateItems: [string, number | null | undefined][] = motionEvent?.rotationRate ?[
-        ['alpha: ', motionEvent.rotationRate?.alpha],
-        ['beta: ', motionEvent.rotationRate?.beta],
-        ['gamma: ', motionEvent.rotationRate?.gamma],
-    ] : []; 
+    if (typeof window.DeviceMotionEvent === 'undefined') {
+        return <WarningMessage>{i18n('DeviceMotionEvent is not supported.')}</WarningMessage>
+    }
 
     return (
         <div class={b()}>
-            {accelerationItems.length ? (
-                <List title={i18n('Acceleration')} items={accelerationItems} />
-            ) : null}
-
-            {accelerationIncludingGravityItems.length ? (
-                <List title={i18n('Acceleration including gravity')} items={accelerationItems} />
-            ) : null}
-
-            {rotationRateItems.length ? (
-                <List title={i18n('Rotation rate')} items={rotationRateItems} />
-            ) : null}
+            <DeviceMotionEvent />
         </div>
     );
 }
