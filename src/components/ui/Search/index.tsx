d1f2a3d9f2bfeb0cb086d84b05f8faa0c76fbc36
Denis Seleznev <hcodes@users.noreply.github.com>
2024-09-22 10:10:48 +0300

    Fix classname

--- src/components/ui/Search/index.tsx
+++ src/components/ui/Search/index.tsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 import { block } from '../../../utils/css/bem';
 import { Input, InputProps } from '../Input';
+import { classname } from '../../../utils/css/classname';
 
 import './index.css';
 
@@ -9,5 +10,6 @@ type SearchProps = InputProps;
 const b = block('search');
 
 export function Search(props: SearchProps) {
-    return <Input class={b()} {...props} />
+    const cls = classname(b(), props.class);
+    return <Input {...props} class={cls} />
 }
