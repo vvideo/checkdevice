477daa573c450b59ab556e9c1fe8242c802b4e30
Denis Seleznev <hcodes@users.noreply.github.com>
2024-10-24 17:57:57 +0300

    Add header

--- src/pages/GpuPage/components/Cube3d/index.tsx
+++ src/pages/GpuPage/components/Cube3d/index.tsx
@@ -9,6 +9,7 @@ const b = block('cube-3d');
 export function Cube3d() {
     return (
         <div class={b()}>
+            <h2 class={b('header')}>CSS 3D Transforms</h2>
             <div class={b('container')}>
                 <div class={b('side', { back: true })}></div>
                 <div class={b('side', { left: true })}></div>
